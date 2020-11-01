790258eb4195217b7a4d3cc1d1eb09c2e2a44ae1
Julie Larkin <julielarkin@Julies-MacBook-Air.local>
2020-11-01 13:46:31 -0800

    Added models, data, linked mySQL

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,56 +1,93 @@
 package org.launchcode.codingevents.controllers;
 
+import org.launchcode.codingevents.data.EventData;
+import org.launchcode.codingevents.data.EventRepository;
+import org.launchcode.codingevents.models.Event;
+import org.launchcode.codingevents.models.EventType;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-/**
- * Created by Chris Bay
- */
 @Controller
 @RequestMapping("events")
 public class EventController {
 
-    //private static List<String> events = new ArrayList<>();
+    @Autowired
+    private EventRepository eventRepository;
 
-/*    @GetMapping
-    public String displayAllEvents(Model model) {
+    // findAll,save, findById
+
+    @GetMapping
+    public String displayAllEvents(Model model){
         model.addAttribute("title", "All Events");
-        model.addAttribute("events", events);
+        model.addAttribute("events", eventRepository.findAll());
         return "events/index";
     }
 
+    //lives at /events/create
     @GetMapping("create")
-    public String displayCreateEventForm(Model model) {
+    public String displayCreateEventForm(Model model){
         model.addAttribute("title", "Create Event");
+        model.addAttribute(new Event());
+        model.addAttribute("types", EventType.values());
         return "events/create";
     }
 
+    //lives at /events/create
     @PostMapping("create")
-    public String processCreateEventForm(@RequestParam String eventName) {
-        events.add(eventName);
+    public String processCreateEventForm(@ModelAttribute @Valid Event newEvent,
+                                         Errors errors, Model model){
+
+        if(errors.hasErrors()){
+            model.addAttribute("title", "Create Event");
+            return "events/create";
+        }
+
+        eventRepository.save(newEvent);
         return "redirect:";
     }
-*/
-    @GetMapping
-    public String displayAllEvents(Model model){
-        Map<String, String> events = new HashMap<>();
-        events.put("Code with Pride", "LGBT Friendly Coding Meetup");
-        events.put("Strange Loop","Loops that are strange");
-        events.put("Apple WWDC","Event for people who like overpriced computers");
 
-        model.addAttribute("events", events);
-
-        return "events/index";
-
-
+    @GetMapping("delete")
+    public String displayDeleteEventForm (Model model){
+        model.addAttribute("title", "Delete Events");
+        model.addAttribute("events", eventRepository.findAll());
+        return "events/delete";
     }
+
+    @PostMapping("delete")
+    public String processDeleteEventForm(@RequestParam(required = false) int[] eventIds) {
+
+        if (eventIds != null) {
+            for (int id : eventIds) {
+                eventRepository.deleteById(id);
+            }
+        }
+        return "redirect:";
+    }
+
+/*    @GetMapping("edit/{eventId}")
+    public String displayEditForm(Model model, @PathVariable int eventId){
+        Event event = EventData.getById(eventId);
+        model.addAttribute("event", event);
+        //model.addAttribute("title", "Edit Event");
+        if (event != null) {
+            model.addAttribute("title", "Edit Event " + event.getName() + " ID " + event.getId());
+        }
+        return "events/edit";
+    }
+
+    @PostMapping("edit")
+    public String processEditForm(int eventId, String name, String description){
+        Event event = EventData.getById(eventId);
+        event.setName(name);
+        event.setDescription(description);
+
+        return "redirect: ";
+    }*/
 }
